d4d408189cc4f9640782c55195b525f38ac1b269
kzkillua <ifeanyi.kz.obinelo@gmail.com>
2024-02-03 21:17:07 -0500

    Update settings (fake)

--- openlearn/settings.py
+++ openlearn/settings.py
@@ -108,7 +108,7 @@ elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
+    } 
 
 
 # Password validation

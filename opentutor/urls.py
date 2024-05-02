b203b70a2034759dee3c5ae571352c7a405a2937
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-05-02 14:39:07 -0400

    Update URL patterns

--- opentutor/urls.py
+++ opentutor/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('documents/', include('documents.urls')),
 ]
+
+if settings.DEVELOPMENT_MODE:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

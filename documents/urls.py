1400a87ab41574c0114dcc919bc3e115bb6d4ae2
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-05-25 22:38:14 -0400

    Add an API endpoint to access flashcards

--- documents/urls.py
+++ documents/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.DocumentList.as_view(), name='document-list'),
     path('<int:pk>', views.DocumentDetail.as_view(), name='document-detail'),
     path('<int:pk>/messages', views.DocumentMessages.as_view(), name='document-messages'),
+    path('<int:pk>/flashcards', views.DocumentFlashcards.as_view(), name='document-flashcards'),
 ]

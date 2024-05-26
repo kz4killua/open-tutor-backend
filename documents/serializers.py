910efccfd4109b35a891fa92da10a2f572ea0825
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-05-26 12:53:26 -0400

    Update serializer to include page count of documents.

--- documents/serializers.py
+++ documents/serializers.py
@@ -5,13 +5,17 @@ from .models import Document, Message, Flashcard
 class DocumentSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     size = serializers.SerializerMethodField()
+    page_count = serializers.SerializerMethodField()
     
     class Meta:
         model = Document
-        fields = ['id', 'name', 'user', 'created', 'file', 'size']
+        fields = ['id', 'name', 'user', 'created', 'file', 'size', 'page_count']
 
     def get_size(self, obj):
         return obj.file.size
+    
+    def get_page_count(self, obj):
+        return len(obj.metadata['page_texts'])
 
 
 class MessageSerializer(serializers.ModelSerializer):
